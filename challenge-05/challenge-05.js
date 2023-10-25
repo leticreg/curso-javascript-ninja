5fd07a55462c399544028c55806b60106837d4cb
leticreg <147549852+leticreg@users.noreply.github.com>
2023-10-25 15:59:52 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = [ 6, 'Leticia', null, undefined, NaN ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction ( arr ) {
+return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log( myFunction( myarray ) [1] );
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction2 ( arr, index ){
+return arr [ index ];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myarray2 = [ 8, 'Regis', false, [2, 7, 9]], { a: 65 }; ];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log( myFunction2 ( myarray2, 0 ));
+console.log( myFunction2 ( myarray2, 1 ));
+console.log( myFunction2 ( myarray2, 2 ));
+console.log( myFunction2 ( myarray2, 3 ));
+console.log( myFunction2 ( myarray2, 4 ));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +55,40 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book ( bookName ){
+var allBooks = {
+    'Latim para Iniciantes':{
+        quantidadePaginas: 657,
+        autor: Mesquita Sales,
+        editora: 'Arety'
+    },
+    'Inglês Simplificado':{
+        quantidadePaginas: 343,
+        autor: Augusto Moura,
+        editora: 'Pascoly'
+    },
+    'Espanhol Nativo':{
+        quantidadePaginas: 120,
+        autor: Suarez Lopez,
+        editora: 'Arepazz'
+    },
+};
+
+return !bookName ? allBooks : allBooks[ bookName ];
+
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log( book() );
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( "O livro Latim para Iniciantes tem ' + book( 'Latim para Iniciantes' ).quantidadePaginas + ' páginas!" );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
